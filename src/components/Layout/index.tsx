b3686f1ef931434d0543e9103bac7fb5ff62fcb9
@MashSHliapa <shliapa.maria@mail.ru>
2024-02-24 21:54:41 +0300

    feat(project): created component Main

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -1,11 +1,13 @@
 import { Outlet } from 'react-router-dom'
 import { Header } from '../Header'
 import './Layout.scss'
+import { Main } from '../Main'
 
 export function Layout() {
   return(
     <div className="Layout">
       <Header/>
+      <Main/>
       <Outlet/>
     </div>
   )
---
+++ src/components/Main/index.tsx
@@ -0,0 +1,29 @@
+// import background from '../../components/images/background.png'
+import background from '../../components/images/background1.png'
+import dowm from '../images/main_down.png'
+import './Main.scss'
+
+export function Main() {
+  return (
+    <div className="main">
+      <div className="main__container _container">
+        <div className="main__body">
+          <h1 className="main__title">ИЗДЕЛИЯ <br /> РУЧНОЙ РАБОТЫ <span>ИЗ ПРИРОДНЫХ МАТЕРИАЛОВ</span></h1>
+          <div className="main__info">
+            <div className="main__text">Сказочна и богата природа. Какие разнообразные чувства переполняют нас при встрече с ней! В любое время года она открывает свои прелести по-разному, давая простор фантазии и творчеству, укрепляя нежные и добрые чувства в человеке. Все эти чувства человек должен пережить в детстве, чтобы потом, с накоплением жизненного опыта</div>
+            <div className="main__button">Каталог</div>
+          </div>
+          <div className="main__down">
+            <img src={dowm} alt="down" />
+          </div>
+
+        </div>
+
+      </div>
+      <div className="main__image _ibg">
+        <img src={background} alt="main" />
+      </div>
+
+    </div>
+  )
+}
