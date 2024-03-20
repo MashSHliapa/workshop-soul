dd4150b05665942dadacbec7ffc59eb3bacb6594
@MashSHliapa <shliapa.maria@mail.ru>
2024-03-20 22:35:29 +0300

    feat(project): add component TradeFairsCard(tablet, mobile)

--- src/components/TradeFairsCard/index.tsx
+++ src/components/TradeFairsCard/index.tsx
@@ -17,31 +17,33 @@ export function TradeFairsCard(props) {
           <div className="info-fairs__date date">
             <div className="date__deadlines">
               <span className='date__deadlines-pretext'>{props.post["date-pretext-from"]}</span>
-              <span className='date__number'>{props.post["number-from"]}</span>{props.post["month-from"]}
+              <span className='date__number'> {props.post["number-from"]}</span>{props.post["month-from"]}
             </div>
             <div className="date__deadlines">
               <span className='date__deadlines-pretext'>{props.post["date-pretext-to"]}</span>
-              <span className='date__number'>{props.post["number-to"]}</span>{props.post["month-to"]}
+              <span className='date__number'> {props.post["number-to"]}</span>{props.post["month-to"]}
             </div>
-            {/* <div className="date__year">{props.post.year}</div> */}
-            <div className="date__year" style={{ marginBottom: '10px' }}>{props.post.year}</div>
-
           </div>
+          <div className="date__year">{props.post.year}</div>
         </div>
 
         <div className="fairs-card__description-fairs description-fairs">
-          <div className="description-fairs__title">{props.post["title-description"]}<br />
-            {props.post["subtitle-description"]}
-          </div>
-          <div className="description-fairs__text"><p>{props.post.text1}</p><p>{props.post.text2}</p>
-          </div>
           <div className="description-fairs__info">
+            <div className="description-fairs__title">{props.post["title-description"]}<br />
+              {props.post["subtitle-description"]}
+            </div>
+            <div className="description-fairs__text"><p>{props.post.text1}</p><p>{props.post.text2}</p>
+            </div>
+          </div>
+
+          <div className="description-fairs__clarifications">
             {props.post['date-description']}<br />
             <b>{props.post['time-description']}</b><br />
-           {props.post['adress-description']}<br />
+            {props.post['adress-description']}<br />
             {props.post.condition}<br />
             <b>{props.post.age}</b><br />
           </div>
+
         </div>
       </div>
     </div>
--- src/pages/TradeFairs/index.tsx
+++ src/pages/TradeFairs/index.tsx
@@ -3,9 +3,9 @@ import { TradeFairsCard } from '../../components/TradeFairsCard'
 import './TradeFairs.scss'
 
 export function TradeFairs() {
-const {data: posts} = useSelector((state )=> state.tradeFairs)
+  const { data: posts } = useSelector((state) => state.tradeFairs)
 
-const tradeFairsPage = posts.map((item) => <TradeFairsCard key={item.id} post={item} />)
+  const tradeFairsPage = posts.map((item) => <TradeFairsCard key={item.id} post={item} />)
 
   return (
     <div className="trade-fairs">
@@ -14,9 +14,8 @@ const tradeFairsPage = posts.map((item) => <TradeFairsCard key={item.id} post={i
           <div className="trade-fairs__title">ярмарки</div>
           <div className="trade-fairs__subtitle">Ярмарки имеют давние традиции, восходящие еще к средневековью. «Где двое, там рынок, трое – базар, а семеро – ярмарка». Это изречение, дошедшее до нас из глубины истории, может даже навести на мысль, что само слово «ярмарка» русского происхождения. Но его германские корни очевидны – jahr-markt – ежегодный рынок, регулярные торжища широкого значения.</div>
           <ul className="cakes__list d-flex flex-wrap justify-content-center gap-5 m-3">
-          {tradeFairsPage}
+            {tradeFairsPage}
           </ul>
-
         </div>
       </div>
     </div>
