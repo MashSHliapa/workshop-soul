1bbcf823ff7368d494d9e713a647545478390a43
@MashSHliapa <shliapa.maria@mail.ru>
2024-08-13 12:33:39 +0300

    feat(project): create json-server for DecorItems

--- src/components/Sections/DecorItems.tsx
+++ src/components/Sections/DecorItems.tsx
@@ -1,13 +1,22 @@
-import { useSelector } from 'react-redux'
+// import { useSelector } from 'react-redux'
 import Slider from 'react-slick'
 import { IconAndTitle } from '../../components/IconAndTitle/IconAndTitle'
-import { RootState } from '../../redux/store'
+// import { RootState } from '../../redux/store'
 import { ItemCard } from '../ItemCard/ItemCard'
 import { sliderSettings } from './sliderSettings'
 import './SectionItems.scss'
+import { useEffect, useState } from 'react'
 
 export function DecorItems() {
-  const { data: posts } = useSelector((state: RootState) => state.decor)
+  // const { data: posts } = useSelector((state: RootState) => state.decor)
+
+  const [posts, setPosts] = useState<{ id: string; image: string; item: string; name: string; size: string; material: string }[]>([])
+
+  useEffect(() => {
+    fetch('http://localhost:8036/decor')
+      .then(res => res.json())
+      .then(data => setPosts(data))
+  }, [])
 
   const decor = posts.map((item) => <ItemCard key={item.id} post={item} />)
 
